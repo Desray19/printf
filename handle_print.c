30987f75b7154e45df62972482a027e840678b5c
robsen123-fexil <111446547+robsen123-fexil@users.noreply.github.com>
2023-10-16 23:56:14 +0300

    committed

--- handle_print.c
+++ handle_print.c
@@ -4,7 +4,7 @@
  * @fmt: Formatted strings in which to print the arguments.
  * @list: List of arguments to be printed.
  * @ind: ind.
- * @buffer: Buffer array to handle print.
+ * @buffer: Buffer arrays to handle print.
  * @flags: Calculate the flags
  * @width: get width.
  * @precision: Precision specification
